f045b2caf3befb30d2b0f352e7aa8d2370c51049
Aleksandr Denissov <137996362+Alex1988Den@users.noreply.github.com>
2025-01-02 12:55:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,9 +108,5 @@ def transfer():
     return "Ошибка: недопустимый формат файлов.", 400
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Использует порт, предоставленный Render
-    app.run(host="0.0.0.0", port=port)  # Слушаем на всех интерфейсах
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Использует порт, предоставленный Render
+    port = int(os.getenv("PORT", 8080)  # Использует порт, предоставленный Render
     app.run(host="0.0.0.0", port=port)  # Слушаем на всех интерфейсах
